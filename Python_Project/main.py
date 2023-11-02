f002fd88d1fd1eceabecff53bda0d58370689aed
baegjh <baegjh@gmail.com>
2023-11-02 15:02:25 +0900

    Git을 사용하여 프로젝트를 진행합니다.

---
+++ Python_Project/main.py
@@ -0,0 +1,13 @@
+# PyQt5를 활용해서 그림판 만들기
+# 1. 화면 크기를 500x500으로 설정
+def draw_canvas():
+
+
+if __name__ == '__main__':
+    draw_canvas()
+    window.mainloop()
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
